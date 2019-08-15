76da85d3a63debe9a6928ba1695d9abc1638c856
Open Close <interfiancee@gmail.com>
2019-08-15 22:43:38 +0800

    Implement Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+package main.java;
+
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+  private Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    String logo = " ____        _        \n"
+        + "|  _ \\ _   _| | _____ \n"
+        + "| | | | | | | |/ / _ \\\n"
+        + "| |_| | |_| |   <  __/\n"
+        + "|____/ \\__,_|_|\\_\\___|\n";
+    System.out.println("Hello from\n" + logo);
+    System.out.println("What can I do for you?");
+
+    Duke duke = new Duke();
+    duke.processUserInputsUntilBye();
+    System.out.println("Bye. Hope to see you again soon!");
+  }
+
+  public Duke() {
+  }
+
+  private void processUserInputsUntilBye() {
+    while (true) {
+      String userInput = scanner.nextLine();
+      if (userInput.equals("bye")) {
+        break;
+      }
+      System.out.println(userInput);
     }
+  }
 }
