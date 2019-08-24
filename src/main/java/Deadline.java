5630985e2ab2240e5458cf488b6918724e5bda27
Wingedevil <jerroldtan@gmail.com>
2019-08-24 13:08:21 +0800

    feat: added date and time parsing

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,13 +1,23 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Deadline extends Task{
     private String date;
+    private Date dateTime;
 
     Deadline(String name, String date){
         super(name);
-        this.date = date;
+        try{
+            dateTime = new SimpleDateFormat("dd/MM/yyyy HHmm").parse(date);
+        }catch (ParseException e){
+            e.printStackTrace();
+            this.date = date;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D][" + getState() + "] " + getName() + " (by: " + date + ")";
+        return "[D][" + getState() + "] " + getName() + " (by: " + (date != null ? date : new SimpleDateFormat("MMM dd yyyy',' hh:mmaa").format(dateTime)) + ")";
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,13 +1,24 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Event extends Task{
     private String date;
+    private Date dateTime;
 
     Event(String name, String date){
         super(name);
-        this.date = date;
+        try{
+            dateTime = new SimpleDateFormat("dd/MM/yyyy HHmm").parse(date);
+        }catch (ParseException e){
+            e.printStackTrace();
+            this.date = date;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E][" + getState() + "] " + getName() + " (at: " + date + ")";
+        return "[E][" + getState() + "] " + getName() + " (at: " + (date != null ? date : new SimpleDateFormat("MMM dd yyyy',' hh:mmaa").format(dateTime)) + ")";
     }
 }
